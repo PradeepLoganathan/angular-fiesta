2bb4cc692997e1cd7f6385f15a2a1af83dd870e6
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2017-09-16 23:57:40 +0530

    router changes

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/Router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([])
   ],
   declarations: []
 })
